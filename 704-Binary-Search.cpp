b15b8a0f4bcc39df7a43e65e15adb01539871281
Ahmed Alaa <ahmd.alaa.kwidia1@gmail.com>
2024-06-02 00:41:55 +0300

    [704] [Time Beats: 94.68%] [Memory Beats: 66.43%] - LeetPush

--- 704-Binary-Search.cpp
+++ 704-Binary-Search.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        if (nums.size() == 1 && nums[0] == target) {
-            return 0;
-        }
-        int r = nums.size() - 1, l = 0;
-        int mid = (r + l) / 2;
-        while (r > l) {
-            mid=(l+r)/2;
-            if (nums[mid] == target) {
+        int r =nums.size()-1;
+        int l = 0;
+        while(l<=r){
+            int mid = (r+l)/2;
+            if(nums[mid]==target){
                 return mid;
-            } else if (nums[mid] < target) {
-                l = mid + 1;
-            } else {
-                r = mid-1;
+            }else if (nums[mid]<target){
+                l = mid+1;
+            }else{
+                r= mid-1;
             }
-        }
-        if(nums[l]==target){
-            return l;
         }
         return -1;
     }
